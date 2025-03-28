e41d9524dd0ed889567bae26e08ad3c294cfdad6
Abdulrahman Alazwari <aazwri@gmail.com>
2025-03-28 23:54:06 +0300

    archive old stater and start new :) starter

--- _starter_old_project/project/settings.py
+++ _starter_old_project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^y*b**%5_#gjkve-g(_pz5cmfwv8h%l#nqm_ev091dlhk!jcj0'
+SECRET_KEY = 'django-insecure-$4i164bo0*9tn!#os7=hv!(re%_-d(dxnsj*vdh+!g%wzn=k(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
